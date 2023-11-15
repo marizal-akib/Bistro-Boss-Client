a9ad5dc0d9bb5fbaea4a0b1647aec61d2b8812e7
marizal-akib <marizalakib@gmail.com>
2023-11-15 14:02:12 +0000

    creat CAPTCHA at login page

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useRef, useState } from 'react';
+import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
 const Login = () => {
+    const captchaRef = useRef(null)
+    const [disabled, setDisabled] = useState(true);
+
+    useEffect(() =>{
+        loadCaptchaEnginge(6);
+    },[])
+
+    const handValidate = () =>{
+        const value = captchaRef.current.value;
+        if(validateCaptcha(value)){
+            setDisabled(false);
+
+        }else{
+            setDisabled(true)
+
+        }
+
+    }
+
   const handleLogin = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -49,8 +70,23 @@ const Login = () => {
                 </a>
               </label>
             </div>
+            <div className="form-control">
+              <label className="label">
+              <LoadCanvasTemplate />
+              </label>
+              <input
+                type="text"
+                ref={captchaRef}
+                name="captcha"
+                placeholder="type the Captcha"
+                className="input input-bordered"
+                required
+              />
+              <button  onClick={handValidate} className='btn btn-outline btn-xs mt-2'>Validate</button>
+            
+            </div>
             <div className="form-control mt-6">
-              <input className="btn btn-primary" type="submit" value="Login" />
+              <input disabled={disabled} className="btn btn-primary" type="submit" value="Login" />
             </div>
           </form>
         </div>
